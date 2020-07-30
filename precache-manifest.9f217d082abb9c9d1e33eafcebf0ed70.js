96c0297af4f921391773e527f989442fa41e90fe
krishan93985 <krisharush93985@gmail.com>
2020-07-30 22:42:35 +0530

    Updates

---
+++ precache-manifest.9f217d082abb9c9d1e33eafcebf0ed70.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c6cd8f7b0ecf89d4ac814dc31c3353f",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "5e661862620379bb9983",
+    "url": "/myportfolio/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "d9555938084a48b82d2e",
+    "url": "/myportfolio/static/css/main.b666d166.chunk.css"
+  },
+  {
+    "revision": "5e661862620379bb9983",
+    "url": "/myportfolio/static/js/2.9836c586.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myportfolio/static/js/2.9836c586.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9555938084a48b82d2e",
+    "url": "/myportfolio/static/js/main.022c1f4a.chunk.js"
+  },
+  {
+    "revision": "bc6701cc98be9f46d7ad",
+    "url": "/myportfolio/static/js/runtime-main.699ab192.js"
+  },
+  {
+    "revision": "b0cde5805822be3bfb5a8ddd3f0f348f",
+    "url": "/myportfolio/static/media/codechef.b0cde580.png"
+  },
+  {
+    "revision": "c9421be52fe48fdf04839b53f8e9f1d4",
+    "url": "/myportfolio/static/media/css3.c9421be5.png"
+  },
+  {
+    "revision": "831c14fcebd3f448a0c3cbc0f69534fc",
+    "url": "/myportfolio/static/media/expressjs.831c14fc.jpeg"
+  },
+  {
+    "revision": "12048fae2b849c0b65216255f11fc4d5",
+    "url": "/myportfolio/static/media/github.12048fae.png"
+  },
+  {
+    "revision": "e7af29fc8e47fa27f5d84406828783cd",
+    "url": "/myportfolio/static/media/githublink.e7af29fc.jpg"
+  },
+  {
+    "revision": "669c92656fd68fb76a66ee776202c309",
+    "url": "/myportfolio/static/media/linkedin.669c9265.jpg"
+  },
+  {
+    "revision": "abdd05128bec137c110687d0c640b9f0",
+    "url": "/myportfolio/static/media/portfolio.abdd0512.png"
+  },
+  {
+    "revision": "def6ba4a1f050ccc8ba1a4c109557be5",
+    "url": "/myportfolio/static/media/postgresql.def6ba4a.gif"
+  },
+  {
+    "revision": "49df7385089ed713b6851d0586097154",
+    "url": "/myportfolio/static/media/profile.49df7385.jpeg"
+  },
+  {
+    "revision": "0bf774137179aa88020e6d598f224ef7",
+    "url": "/myportfolio/static/media/react.0bf77413.png"
+  },
+  {
+    "revision": "4912f6d800e1b75354e0b5d4794e1856",
+    "url": "/myportfolio/static/media/redux.4912f6d8.png"
+  },
+  {
+    "revision": "5c7157f3bc2ba68367b5b3c474b47da7",
+    "url": "/myportfolio/static/media/smartbrain.5c7157f3.png"
+  }
+]);
