ce1661bd501f5b6e9f2c16f7e3098473023af8c2
krishan93985 <krisharush93985@gmail.com>
2021-06-15 11:43:16 +0530

    deployment env bug fix

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -13,6 +13,7 @@ function App() {
   const [loaded, setload] = useState(false);
 
   const invokeServer = async () => {
+    console.log(process.env.REACT_APP_BACKEND_URL)
     const jsonServerData = await fetch(`${process.env.REACT_APP_BACKEND_URL}`,{method:'get', headers:{'Content-type':'application/json'}});
     const serverData = await jsonServerData.json();
     console.log("Backend Server Says ",serverData);
